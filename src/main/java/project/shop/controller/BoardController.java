ca42a9fa4ac7a0b3cf7855052475555b13a034b4
hi <lsmunt2@gmail.com>
2022-08-16 18:03:23 +0900

    reviews CRUD

--- src/main/java/project/shop/controller/BoardController.java
+++ src/main/java/project/shop/controller/BoardController.java
@@ -1,17 +1,125 @@
 package project.shop.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import project.shop.service.BoardService;
+import vo.BoardVO;
 
 @Controller
 public class BoardController {
+	
+	String loginId = "임시";
 	
 	@Autowired
 	private BoardService service;
 	public BoardController(BoardService service) {
 		this.service = service;
+	}
+	
+	@GetMapping("/reviews")
+	public ModelAndView board(@RequestParam(defaultValue="1")int page) {
+		ModelAndView mv = new ModelAndView();
+		int bType=2;
+		mv.addObject("bPage", service.makeBoardPage(bType, page));
+
+		mv.setViewName("reviews/list");
+		return mv;
+	}
+	
+//	@PostMapping("/reviews/search")
+//	public ModelAndView boardSearch(@RequestParam(defaultValue = "1")int page, String search) {
+//		ModelAndView mv = new ModelAndView();
+//		int bType=2;
+//		mv.addObject("bPage", service.boardSearch(bType, search, search, page));
+//		mv.setViewName("reviews/search");
+//		return mv;
+//	}
+	
+	@GetMapping("/reviews/write")
+	public String writeForm() {
+		if(loginId != null) {
+			return "reviews/write";			
+		} else {
+//			return "login";
+			return "";
+		}
+	}
+	@PostMapping("/reviews/write")
+	public ModelAndView write(BoardVO board) {
+		ModelAndView mv = new ModelAndView();
+		int result;
+		if(loginId != null) {
+			result = service.write(board, loginId, 2);
+		} else {
+			result = 0;
+			mv.addObject("message","로그인이 필요합니다.");
+			mv.setViewName("login");
+		}
+		mv.addObject("result", result);
+		mv.setViewName("redirect:/reviews");
+		return mv;
+	}
+	
+	@GetMapping("/reviews/read")
+	public ModelAndView read(@RequestParam("bNum")int bNum, HttpSession session) {
+		ModelAndView mv = new ModelAndView();
+		BoardVO board = service.readNoCount(bNum);
+		service.read(bNum, loginId);
+		service.readNoCount(bNum);
+		mv.addObject("board", board);
+		mv.addObject("loginId", loginId);
+		mv.setViewName("reviews/read");
+		return mv;
+	}
+	
+	@GetMapping("reviews/update")
+	public ModelAndView updateForm(int bNum, HttpSession session) {
+		ModelAndView mv = new ModelAndView();
+		BoardVO board = service.readNoCount(bNum);
+		mv.addObject("original", board);
+//		String loginId = (String)session.getAttribute("loginId");
+		if(board.getbWriter().equals(loginId)) {
+			mv.setViewName("reviews/update");
+		} else {
+//			mv.setViewName("login");
+			mv.setViewName("");
+		}
+		return mv;
+	}
+	
+	@PostMapping("reviews/update")
+	public String update(BoardVO board, HttpSession session, Model model) {
+//		String loginId = (String)session.getAttribute("loginId");
+		boolean result = service.update(board, loginId);
+		if (result) {
+			return "redirect://localhost:8080/reviews/read?bNum="+board.getbNum();
+		} else {
+			model.addAttribute("bNum", board.getbNum());
+			return "reviews/update-fail";
+		}
+		
+	}
+	
+	@GetMapping("reviews/delete")
+	public ModelAndView delete(int bNum, HttpSession session) {
+		ModelAndView mv = new ModelAndView();
+//		String loginId = (String)session.getAttribute(loginId);
+		Boolean result = service.delete(bNum, loginId);
+		if(result) {
+			mv.setViewName("redirect:/reviews");
+		} else {
+			mv.addObject("bNum", bNum);
+			mv.setViewName("revides/delete-fail");
+		}
+		return mv;
 	}
 	
 }
--- src/main/java/vo/BoardVO.java
+++ src/main/java/vo/BoardVO.java
@@ -1,5 +1,66 @@
 package vo;
 
-public class BoardVO {
+import java.util.Date;
 
+public class BoardVO {
+	
+	private int bNum;
+	private int bType;
+	private String bTitle;
+	private String bContent;
+	private String bWriter;
+	private Date bWriteDate;
+	private int bReadCount;
+	
+	public int getbNum() {
+		return bNum;
+	}
+	public void setbNum(int bNum) {
+		this.bNum = bNum;
+	}
+	public int getbType() {
+		return bType;
+	}
+	public void setbType(int bType) {
+		this.bType = bType;
+	}
+	public String getbTitle() {
+		return bTitle;
+	}
+	public void setbTitle(String bTitle) {
+		this.bTitle = bTitle;
+	}
+	public String getbContent() {
+		return bContent;
+	}
+	public void setbContent(String bContent) {
+		this.bContent = bContent;
+	}
+	public String getbWriter() {
+		return bWriter;
+	}
+	public void setbWriter(String bWriter) {
+		this.bWriter = bWriter;
+	}
+	public Date getbWriteDate() {
+		return bWriteDate;
+	}
+	public void setbWriteDate(Date bWriteDate) {
+		this.bWriteDate = bWriteDate;
+	}
+	public int getbReadCount() {
+		return bReadCount;
+	}
+	public void setbReadCount(int bReadCount) {
+		this.bReadCount = bReadCount;
+	}
+	
+	@Override
+	public String toString() {
+		return "BoardVO [bNum=" + bNum + ", bType=" + bType + ", bTitle=" + bTitle + ", bContent=" + bContent
+				+ ", bWriter=" + bWriter + ", bWriteDate=" + bWriteDate + ", bReadCount=" + bReadCount + "]";
+	}
+	
+	
+	
 }
